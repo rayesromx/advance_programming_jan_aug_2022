20b7e8be2d5927db30bd51e97302f3ae647723a3
ray.esro <ray.esro@gmail.com>
2022-03-09 21:12:53 -0600

    Avances vistos en clase

--- BlankScreenFromScratch/resource.h
+++ BlankScreenFromScratch/resource.h
@@ -1,14 +1,20 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by 06_BlankScreenFromScratch.rc
+//
+#define IDD_INICIO_SESION               9
+#define IDC_EDIT1                       1002
+#define IDC_EDIT2                       1003
+#define IDBTN_INICIAR_SESION            1004
+#define IDLBL_USUARIO                   1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
