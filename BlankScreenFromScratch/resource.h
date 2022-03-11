2fb197b6c8bc0237fa0cbe6ec0f2926a20316e45
ray.esro <ray.esro@gmail.com>
2022-03-11 17:57:53 -0600

    cambios vistos en clase

--- BlankScreenFromScratch/resource.h
+++ BlankScreenFromScratch/resource.h
@@ -1,14 +1,23 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by 06_BlankScreenFromScratch.rc
+//
+#define IDD_INICIO_SESION               101
+#define IDD_VENTANA_PRINCIPAL           103
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON2                     1003
+#define IDBTN_INICIAR_SESION            1003
+#define IDC_BUTTON3                     1004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
