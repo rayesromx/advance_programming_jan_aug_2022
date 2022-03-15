4956620119efd5978c33a283b09ca413f439c89d
ray.esro <ray.esro@gmail.com>
2022-03-14 20:22:25 -0600

    Codigo para desplegar una primera pantalla de Login

--- 07_WinAPI/07_WinAPI.cpp
+++ 07_WinAPI/07_WinAPI.cpp
@@ -3,22 +3,65 @@
 #endif 
 
 #include <windows.h>
+#include "resource.h"
+
+HINSTANCE GlobalAppHInstance;
 
 LRESULT CALLBACK DefaultWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK fLoginProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
+    GlobalAppHInstance = hInstance;
 
-    // Run the message loop.
+    //Creamos una dialogo a partir de la plantilla IDD_LOGIN
+    HWND handlerLoginWindow = CreateDialogW(
+        GlobalAppHInstance,//La instancia de nuestra aplicacion
+        MAKEINTRESOURCE(IDD_LOGIN),//El ID del dialogo que vamos a crear
+        NULL, //Como esta es la primera ventana, no tiene una ventana padre
+        fLoginProc //La funcion Window Procedure asociada a este dialogo
+    );
+
+    //validamos que no haya habido errores
+    if (handlerLoginWindow == NULL)
+    {
+        MessageBox(NULL,
+            L"Hubo un error al crear la aplicacion. Contacte al administrador o soporte.",
+            L"ERROR FATAL", 
+            MB_OK | MB_ICONERROR
+            );
+        return 0;
+    }
+
+    //Mostramos la ventana y listo, hasta aqui llega todo el codigo interesante del main
+    //en este ejercicio
+    ShowWindow(handlerLoginWindow, SW_NORMAL);
+
+    // Recuerden que el message loop siempre debe de estar
 
     MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0))
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
     {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
     return 0;
+}
+
+LRESULT CALLBACK fLoginProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+
+        return 0;
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 LRESULT CALLBACK DefaultWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -28,10 +71,6 @@ LRESULT CALLBACK DefaultWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
-
-  
-    return 0;
-
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
---
+++ 07_WinAPI/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 07_WinAPI.rc
+//
+#define IDBTN_LOGIN_INICIARSESION       3
+#define IDD_LOGIN                       101
+#define IDTXT_LOGIN_USUARIO             1001
+#define IDBTN_LOGIN_REGISTRAR           1002
+#define IDBTN_SALIR_LOGIN               1003
+#define IDBTN_LOGIN_SALIR               1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
