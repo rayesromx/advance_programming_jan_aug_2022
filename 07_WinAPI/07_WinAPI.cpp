a617e96031884f94f2acd2a13b2c4f2daf803a7d
ray.esro <ray.esro@gmail.com>
2022-03-14 20:33:15 -0600

    Se agrega codigo para cerrar la aplicacion

--- 07_WinAPI/07_WinAPI.cpp
+++ 07_WinAPI/07_WinAPI.cpp
@@ -7,6 +7,8 @@
 
 HINSTANCE GlobalAppHInstance;
 
+bool exitProgram = false;
+
 LRESULT CALLBACK DefaultWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK fLoginProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -53,13 +55,46 @@ LRESULT CALLBACK fLoginProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
+    /* Este evento remueve el dialog de la pantalla 
+    * Sent when a window is being destroyed. It is sent to the window procedure of the window being destroyed 
+      after the window is removed from the screen.
+      https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-destroy
+    * */
     case WM_DESTROY:
-        PostQuitMessage(0);
+        if (exitProgram) {
+            PostQuitMessage(16);
+        }
         return 0;
 
-
-        return 0;
-
+    /*Codigo para manejar eventos de entrada
+    * Sent when the user selects a command item from a menu, when a control sends a notification message to its parent window, 
+    * or when an accelerator keystroke is translated.
+    * https://docs.microsoft.com/en-us/windows/win32/menurc/wm-command
+    * */
+    case WM_COMMAND: {
+        /*
+          wParam
+            The LOWORD contains the button's control identifier. The HIWORD specifies the notification code.
+            https://docs.microsoft.com/en-us/windows/win32/controls/bn-clicked
+          lParam
+            A handle to the button.
+        */
+        switch (LOWORD(wParam))
+        {
+        case IDBTN_LOGIN_SALIR: {
+            //freeMemoryUser();
+            exitProgram = true;
+            DestroyWindow(hwnd);
+        }break;
+        }
+    }break;
+    /*Este evento se dispara cuando le picamos al boton X para cerrar
+    * Sent as a signal that a window or an application should terminate. 
+    * https://docs.microsoft.com/en-us/windows/win32/winmsg/wm-close
+    */
+    case WM_CLOSE:
+        
+        break;
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
