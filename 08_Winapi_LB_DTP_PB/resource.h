c13effcb5ef066f8e272a38735003d2f1cf0355e
ray.esro <ray.esro@gmail.com>
2022-03-20 22:34:26 -0600

    Ejercicio base de listbox y picture control

---
+++ 08_Winapi_LB_DTP_PB/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 08_Winapi_LB_DTP_PB.rc
+//
+#define IDD_MAINDIALOG                  7
+#define IDD_POKEFORM                    103
+#define IDLB_POKELIST                   1002
+#define IDBTN_ADDPKMN                   1004
+#define IDBTN_UPDATEPKMN                1005
+#define IDBTN_REMOVEPKMN                1006
+#define IDLBL_NAME                      1008
+#define IDLBL_NUMBER                    1009
+#define IDLBL_TYPE                      1010
+#define IDLBL_APPEARENCE                1011
+#define IDPC_PHOTO                      1012
+#define IDTXT_NAME                      1013
+#define IDTXT_NUMBER                    1014
+#define IDTXT_TYPE                      1015
+#define IDTXT_APPEARENCE                1016
+#define IDBTN_ACCEPT                    1017
+#define IDBTN_LOADIMG                   1018
+#define PB_FOTOP1M                      1019
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
