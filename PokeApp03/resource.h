d97824823c54af87704522090ebced7bd04e88a1
ray.esro <ray.esro@gmail.com>
2022-03-24 21:11:19 -0600

    Aplicacion Pokeapp vista en clase

---
+++ PokeApp03/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PokeApp03.rc
+//
+#define IDBTN_UPDATE                    3
+#define IDBTN_REMOVE                    4
+#define IDD_POKEAPP                     2101
+#define IDD_ADDNEWPKMN                  2103
+#define IDC_LIST1                       21001
+#define IDLB_POKELIST                   21001
+#define IDC_EDIT1                       21002
+#define IDC_EDIT2                       21003
+#define IDC_EDIT3                       21004
+#define IDBTN_ADD                       21005
+#define IDTXT_PKMNNAME                  21006
+#define IDTXT_PKMNTYPE                  21007
+#define IDTXT_PKMNNUMBER                21008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
